7c3255835c063f348a32b3134970641cb0c25ebe
battaillon <michelhansbatta@gmail.com>
2022-04-06 01:49:03 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// ENSPD_GIT_GLO4
+//Console.WriteLine("Hello, World!");
+//simple calculator in c# by BATTA MICHEL HANS;
+ class MyApp
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            string operand;
+            ConsoleKeyInfo status;
+            float answer;
+
+            while (true)
+            {
+                Console.WriteLine("Hello, MR NOUMBO!");
+                Console.Write("Please enter the first integer: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Please enter the second integer: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Please enter an operand (+, -, /, *): ");
+                operand = Console.ReadLine();
+
+                switch (operand)
+                {
+                    case "-":
+                        answer = num1 - num2;
+                        break;
+                    case "+":
+                        answer = num1 + num2;
+                        break;
+                    case "/":
+                        answer = num1 / num2;
+                        break;
+                    case "*":
+                        answer = num1 * num2;
+                        break;
+                    default:
+                        answer = 0;
+                        break;
+                }
+
+                Console.WriteLine(num1.ToString() + " " + operand + " " + num2.ToString() + " = " + answer.ToString());
+                Console.WriteLine("\n\n Do You Want To Break (Y/y)");
+                status = Console.ReadKey();
+                if(status.Key==ConsoleKey.Y)
+                {
+                    break;
+                }
+                Console.Clear();
+            }
+        }
+    }
